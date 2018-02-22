a7b1f90408758e72a0348716764076abbf2b0e95
guowentao <gwem.win@gmail.com>
2018-02-22 16:42:09 +0800

    test

---
+++ pages/index/index.js
@@ -0,0 +1,59 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    showRating:false,
+    showWish:false,
+    acquiredSelected:false,   // 精选榜单只请求一次
+    inTheaters:{},   //影院热映
+    comingSoon:{},   //精选榜单
+    top250:{},   //豆瓣Top250
+    weekly:{},   //口碑榜
+    newMovie:{},   //新片榜
+    usBox:{}    //票房榜
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
