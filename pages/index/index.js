ab27ab3d329c88c5f18845a7ac6a062aef33e115
guowentao <gwem.win@gmail.com>
2018-02-22 18:02:45 +0800

    data

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,56 +4,113 @@ const app = getApp()
 
 Page({
   data: {
-    showRating:false,
-    showWish:false,
-    acquiredSelected:false,   // 精选榜单只请求一次
-    inTheaters:{},   //影院热映
-    comingSoon:{},   //精选榜单
-    top250:{},   //豆瓣Top250
-    weekly:{},   //口碑榜
-    newMovie:{},   //新片榜
-    usBox:{}    //票房榜
+    showRating: false,
+    showWish: false,
+    acquiredSelected: false,   // 精选榜单只请求一次
+    inTheaters: {},   //影院热映
+    comingSoon: {},   //精选榜单
+    top250: {},   //豆瓣Top250
+    weekly: {},   //口碑榜
+    newMovie: {},   //新片榜
+    usBox: {}    //票房榜
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  onLoad: function (options) {
+    // 页面初始化，options为页面跳转所带过来的参数
+    var inTheatersURL = app.globalData.doubanBase + app.globalData.inTheaters + "?start=0&&count=10";
+    var comingSoonURL = app.globalData.doubanBase + app.globalData.comingSoon + "?start=0&&count=10";
+
+    console.log(inTheatersURL, comingSoonURL);
+
+    this.getMovieListData(inTheatersURL, "inTheaters", "影院热映");
+    this.getMovieListData(comingSoonURL, "commingSoon", "精选榜单");
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  getMovieListData: function (url, settedKey, categoryTitle) {
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      duration: 10000
+    });
+    var that = this;
+    // 请求电影数据
+    wx.request({
+      url: url,
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {
+        "content-type": "json"
+      }, // 设置请求的 header
+      success: function (res) {
+        // success
+        var data = res.data;
+        // 组装电影数据
+        that.processMovieListData(data, settedKey, categoryTitle)
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+        wx.hideToast();
+      }
+    })
+  },
+  // 处理电影数据
+  processMovieListData: function (data, settedKey, categoryTitle) {
+    var movies = [];
+    for(let idx in data.subjects){
+      var subject = data.subjects[idx];
+      var showRating = false;
+      var showWish = false;
+      if('inTheaters' == settedKey){
+        showRating = true;
+        showWish = false;
+      }else{
+        showRating = true;
+        showWish = false;
+      }
+      var temp = {
+        id: subject.id,
+        title: subject.title,
+        rating:subject.rating,
+        collect_count:subject.collect_count,
+        images:subject.images,
+        subtype:subject.subtype,
+        directors:subject.directors,
+        casts:subject.casts,
+        year:subject.year,
+        showRating:showRating,
+        showWish:showWish
+      };
+      movies.push(temp);
+    };
+    var readyData = {};
+    readyData[settedKey] = {
+      categoryTitle : categoryTitle,
+      movies:movies
+    };
+    console.log(readyData);
+    this.setData(readyData);
+  },
+  // 滑动屏幕
+  handleTouchMove:function(event){
+    var offsetTop = event.target.offsetTop;
+    console.log('handleTouchMove offsetTop:' + offsetTop);
+    if(offsetTop > 10 && !this.data.acquiredSelected){
+      this.getSelectedListData();
+    }
   }
 })
