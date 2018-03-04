58ea41f4d5a65b4d787073fa0ca99e4ede87fb58
Gwemz <3396543978@qq.com>
2018-03-04 16:38:34 +0800

    poster

--- pages/movie-detail/movie-poster/movie-poster.js
+++ pages/movie-detail/movie-poster/movie-poster.js
@@ -1,5 +1,19 @@
 // pages/movie-detail/movie-poster/movie-poster.js
 var app = getApp();
 Page({
+    data:{
 
+    },
+    onLoad:function(options){
+        // 页面初始化，options为页面跳转所带来的参数
+        var that = this;
+        var posterUrl = options.posterUrl;
+        var readyData = {"posters":[posterUrl]};
+        console.log(posterUrl,readyData);
+        this.setData(readyData);
+        that.setData({
+            "windowWidth":app.globalData.windowWidth,
+            "windowHeight":app.globalData.windowHeight
+        })
+    }
 })
